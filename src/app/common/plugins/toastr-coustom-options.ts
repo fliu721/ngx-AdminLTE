cd9aabc0688b60252c867d6696cd02ca64d7525d
F.liu <f.liu721@gmail.com>
2017-05-10 22:31:19 +0800

    delete toastr-coustom-options.ts

--- src/app/common/plugins/toastr-coustom-options.ts
+++
@@ -1,9 +0,0 @@
-import { ToastOptions } from 'ng2-toastr';
-export class ToastrCustomOption extends ToastOptions {
-  animate = 'fade'; // fade flyLeft flyRight
-  newestOnTop = false;
-  showCloseButton = true;
-  dismiss = 'auto'; // auto click controlled
-  toastLife = 3000; // 3秒后自动消失
-
-}
